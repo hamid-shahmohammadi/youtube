d5c1618009da9953c3de8012cdcde0344ad5946e
Hamid Shah Mohammadi <info@iranlaravel.ir>
2019-05-08 12:30:05 +0330

    subscribe

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,11 @@
-<?php require_once("includes/config.php"); ?>
+<?php 
+require_once("includes/config.php"); 
+require_once("includes/classes/User.php"); 
+require_once("includes/classes/Video.php"); 
+
+$usernameLoggedIn = User::isLoggedIn() ? $_SESSION["userLoggedIn"] : "";
+$userLoggedInObj = new User($con, $usernameLoggedIn);
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,6 +18,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
     <script src="assets/js/commonActions.js"></script>
+    <script src="assets/js/userActions.js"></script>
 
 </head>
 <body>
